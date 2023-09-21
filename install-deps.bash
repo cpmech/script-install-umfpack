7f05cffb755ded6090429a1cc491b4ceff3f644e
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-21 17:00:16 +1000

    Init

---
+++ install-deps.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# fake sudo function to be used by docker build
+sudo () {
+  [[ $EUID = 0 ]] || set -- command sudo "$@"
+  "$@"
+}
+
+# install packages
+sudo apt-get update -y && \
+sudo apt-get install -y --no-install-recommends \
+    cmake \
+    g++ \
+    gdb \
+    gfortran \
+    git \
+    libgmp-dev \
+    libmetis-dev \
+    libmpfr-dev \
+    libopenblas-dev \
+    make \
+    patch
